fca983fd9e4137d1b652c414d72df07ec142c547
tuan-ng <iam.tuan.ng@gmail.com>
2019-09-21 12:05:11 -0400

    Updates

---
+++ precache-manifest.2c36db0e29c3db33298d1dc36968895b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed367a8d4d3b55cad79625921c2abf9",
+    "url": "/codeflask-arith-js/index.html"
+  },
+  {
+    "revision": "2bec633a21967f8ee542",
+    "url": "/codeflask-arith-js/static/css/main.81684183.chunk.css"
+  },
+  {
+    "revision": "d5e003558e3f32944ee2",
+    "url": "/codeflask-arith-js/static/js/2.72354540.chunk.js"
+  },
+  {
+    "revision": "2bec633a21967f8ee542",
+    "url": "/codeflask-arith-js/static/js/main.98dfb7d0.chunk.js"
+  },
+  {
+    "revision": "ea2c2bf0077345653de6",
+    "url": "/codeflask-arith-js/static/js/runtime-main.9428ffab.js"
+  }
+]);
